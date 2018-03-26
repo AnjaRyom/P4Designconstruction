1169945d9c88b007fb3a9c7289c2bc6366d39bc6
Unknown <anila_khaliq@hotmail.com>
2018-03-26 12:46:30 +0200

    Changed names in product section. Created new classes.
    Names in product section are changed to make it easy in understanding the code. 
    New classes have been created. The classes are in Product section and are: Service, ArticleInUse and Article.

---
+++ src/com/company/Inventory/Article.java
@@ -0,0 +1,81 @@
+package com.company;
+
+
+/* The Article class contains info about the single product
+ * it can be edited by using the constructor but only location
+ * and retailPrice can be edited when it already exist (by setters).
+ */
+
+public class Article extends Product {
+    private String brand;
+    private String category;
+    private String name;
+    private String partNo;
+    private Location location;
+    private double purchasePrice;
+    private double retailPrice;
+
+    //Constructor
+    public Article(String brand,
+                   String category,
+                   String name,
+                   String partNo,
+                   int location,
+                   double purchasePrice,
+                   double retailPrice) {
+        this.brand = brand;
+        this.category = category;
+        this.name = name;
+        this.partNo = partNo;
+        this.location = location;
+        this.purchasePrice = purchasePrice;
+        this.retailPrice = retailPrice;
+    }
+
+    //Getters
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPartNo() {
+        return partNo;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+
+    //Setters - those without setter cannot be edited by other than the object itself
+
+    /**
+     * Change article location
+     * @param location New article location
+     */
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+}
+
+
+
--- src/com/company/Inventory/Inventory.java
+++ src/com/company/Inventory/Inventory.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Dictionary;
 
-/** Inventory aggregates the class products and keeps track of the inventory.
+/** Inventory aggregates the class article and keeps track of the inventory.
  * It tells whether the stock is low.
  * if the stock is too low, it tells if the article has been ordered or not.
  * This class DOES NOT order new parts. */
@@ -9,13 +9,13 @@ import java.util.Dictionary;
 public class Inventory {
 
     /* Map is a type that takes key, K and value, V as parameters.
-     * In this case the key is the product (which is both a type and a class we created ourselves),
-     * and the value is the number of products in stock. */
-    private Map<Product, int> numberInStock; // this represents the number of a certain product in stock
-    private Map<Product, int> lowStock; // this represents the case where the stock is too low - because we will keep track of when a product is low in stock
+     * In this case the key is the article (which is both a type and a class we created ourselves),
+     * and the value is the number of articles in stock. */
+    private Map<Article, int> numberInStock; // this represents the number of a certain article in stock
+    private Map<Article, int> lowStock; // this represents the case where the stock is too low - because we will keep track of when an article is low in stock
 
     // This is the start if there is already an existing stock.
-    public Inventory(Map<Product, int> numberInStock, Map<Product, int> lowStock) {
+    public Inventory(Map<Article, int> numberInStock, Map<Article, int> lowStock) {
         this.numberInStock = numberInStock;
         this.lowStock = lowStock;
     }
@@ -28,10 +28,10 @@ public class Inventory {
 
 
     /* NEXT STEPS:
-    - Adding indicect accessors: addProduct and removeProduct
-     (like getters and setters, for the purpose of adding and removing products to the inventory)
-    - clearStockAlert value - for removing automatic ordering from a product that is already added
-    - setStockAlertValue - for specifying when (the hairdresser/us) decides that a product is low in stock
+    - Adding indicect accessors: addArticle and removeArticle
+     (like getters and setters, for the purpose of adding and removing articles to the inventory)
+    - clearStockAlert value - for removing automatic ordering from an article that is already added
+    - setStockAlertValue - for specifying when (the hairdresser/us) decides that an article is low in stock
     - Do something that makes it possible to notify stakeholders when a change in the inventory has happened.
     - Read notes (observer pattern page 272)
      */
@@ -39,51 +39,51 @@ public class Inventory {
 
 
     /* method overloading = two functions can have the same name
-     * - in the cases below (addProduct and removeProduct) , if we call both parameter product and parameter number
-     * the first function will be called, if we only have a parameter, product the second method will be used. */
+     * - in the cases below (addArticle and removeArticle) , if we call both parameter article and parameter number
+     * the first function will be called, if we only have a parameter, article the second method will be used. */
 
     // method signature - fill out functionality
     /**
-     * This adds a number of product to the inventory
-     * @param p Product
-     * @param count How many products are added
+     * This adds a number of articles to the inventory
+     * @param a Article
+     * @param count How many articles are added
      */
-    public void addProduct(Product p, int count){
+    public void addArticle(Article a, int count){
     }
 
     /**
-     * Finished - adds one product
-     * @param p Product
+     * Finished - adds one article
+     * @param a Article
      */
-    public void addProduct(Product p){
-        this.addProduct(p, 1);
-    }
-
-    //Fill out functionality
-    /**
-     * This removes a number of products from the inventory
-     * @param p Product
-     * @param count How many products are removed
-     */
-    public void removeProduct(Product p, int count){
-
-    }
-
-    /**
-     * Finished - removes one product
-     * @param p Product
-     */
-    public void removeProduct (Product p){
-        this.removeProduct(p,1);
+    public void addArticle(Article a){
+        this.addArticle(a, 1);
     }
 
     //Fill out functionality
     /**
-     * This method sets the number of when a product is low in stock - perhaps it should be renamed
-     * @param p Product
+     * This removes a number of articles from the inventory
+     * @param a Article
+     * @param count How many articles are removed
+     */
+    public void removeArticle(Article a, int count){
+
+    }
+
+    /**
+     * Finished - removes one article
+     * @param p Article
+     */
+    public void removeArticle(Article a){
+        this.removeArticle(p,1);
+    }
+
+    //Fill out functionality
+    /**
+     * This method sets the number of when an article is low in stock - perhaps it should be renamed
+     * @param a Article
      * @param count Minimum number
      */
-    public void SetStockAlertValue (Product p, int count){
+    public void SetStockAlertValue (Article a, int count){
     }
 
 }
