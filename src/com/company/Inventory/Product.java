a36f61f719e5ad85cbcc9d02e6fb9b367304966f
Anila Khaliq <anila_khaliq@hotmail.com>
2018-03-26 13:28:55 +0200

    Product class variables made

--- src/com/company/Inventory/Product.java
+++ src/com/company/Inventory/Product.java
@@ -1,5 +1,36 @@
 package com.company;
 
+//This class is a super-class for; Service, ArticleInUse and Article.
 public abstract class Product{
+    private String category;
+    private String name;
+    private double purchasePrice;
 
+    //Constructors
+    public Product(String category, String name, double purchasePrice) {
+        this.category = category;
+        this.name = name;
+        this.purchasePrice = purchasePrice;
+    }
+
+    //Getters
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    //Setters
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
 }
+
+
+
