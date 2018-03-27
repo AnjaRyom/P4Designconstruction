82cf4270eb2b3c2ebb408e5ca3ba5cb85bc593ff
Anila Khaliq <anila_khaliq@hotmail.com>
2018-03-27 11:27:13 +0200

    Appointment class added and partly defined

---
+++ src/com/company/Inventory/Appointment.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**The inputs, that appointment takes are Employee and Customer. Furthermore it aggregates Service.
+ * It has a relation to the Cash drawer.
+ * An appointment consists of several services and employees.
+ * It aggregates to the Calendar.
+ * */
+public class Appointment{
+    private double duration; //it's possible for the employee to set the duration themselves. It should calculate the time, if more treatments are done.
+    private Service service;
+
+
+    //Constructors
+
+
+    //Getters and setters
+
+}
