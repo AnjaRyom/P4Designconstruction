f8fcd2849c164434a9ecef49ef7664683ecd8aad
Jessern <jensenjessicac@gmail.com>
2018-04-24 12:46:20 +0200

    Begin initial work on payment

---
+++ src/IxD406/Payment/Main.java
@@ -0,0 +1,16 @@
+package IxD406.Payment;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
