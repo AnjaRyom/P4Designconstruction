249409ef4312bc65fe004a503ebdda960f84d35d
Jessern <jensenjessicac@gmail.com>
2018-04-30 11:13:26 +0200

    Påbegyndt SummaryView
    Ny klasse InvoiceView er skabt med getters og setters til tableviewet, der nu skal laves.

---
+++ src/IxD406/Payment/View/InvoiceView.java
@@ -0,0 +1,45 @@
+package IxD406.Payment.View;
+
+public class InvoiceView {
+
+    private String name;
+    private int quantity;
+    private double price;
+
+    public invoiceViewConst1() {
+        this.name = "";
+        this.quantity = 0;
+        this.price = 0;
+    }
+
+    public invoiceViewConst2(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
